41ac41c204ebe5037dd655157e62bcafd9bf2771
Mahmoud Yousif <myousif123@gmail.com>
2025-11-17 18:12:28 -0500

    fix: Removed commented out mock data

--- src/app/api/cities/route.ts
+++ src/app/api/cities/route.ts
@@ -2,29 +2,6 @@ import db from '../../../db'
 import { advocates } from '@/db/schema';
 
 export async function GET() {
-  // const cities = [
-  //   'New York, NY',
-  //   'San Francisco, CA',
-  //   'Austin, TX',
-  //   'Boston, MA',
-  //   'Seattle, WA',
-  //   'Denver, CO',
-  //   'Chicago, IL',
-  //   'Los Angeles, CA',
-  //   'Miami, FL',
-  //   'Atlanta, GA',
-  //   'Portland, OR',
-  //   'Minneapolis, MN',
-  //   'Nashville, TN',
-  //   'Phoenix, AZ',
-  //   'Philadelphia, PA',
-  //   'Washington, DC',
-  //   'San Diego, CA',
-  //   'Dallas, TX',
-  //   'Houston, TX',
-  //   'Charlotte, NC',
-  // ];
-
   const data = await db.selectDistinct({
       city: advocates.city,
     })
